0c9a737ba55481124ff4a64936bb751efed75fd4
ben <bencrane5.12@gmail.com>
2019-05-20 23:12:04 -0400

    Create search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="findMeteorites">
+      <input
+        v-model="searchText"
+        type="text"
+        placeholder="Enter search terms"
+      />
+      <button>Search</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  data() {
+    return {
+      searchText: ""
+    };
+  },
+  props: {
+    previousSearches: {
+      type: Array
+    }
+  },
+  methods: {
+    findMeteorites() {
+      alert(this.searchText);
+    }
+  }
+};
+</script>
+
+<style scoped>
+* {
+  font-size: 1.1em;
+}
+button {
+  text-transform: uppercase;
+}
+</style>
